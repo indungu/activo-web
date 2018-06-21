a69fa6496973ed885feb0d1470d472f57c624b08
Ayelegun Kayode Michael <ayelegun.kayode@gmail.com>
2018-06-21 22:24:36 +0100

    chore(authentication): fix dropdown for user logout (#30)
      - change dropdown component to a class based component
      - re-write method handlers to listen for clicks outside the dropdown component
    
    [Maintains #158440552]

--- src/components/ProfileDropDown/ProfileDropDown.test.tsx
+++ src/components/ProfileDropDown/ProfileDropDown.test.tsx
@@ -8,18 +8,23 @@ import { shallow } from 'enzyme';
 import ProfileDropDown from './';
 
 describe('<ProfileDropDown />', () => {
-  const props = {
-    hidden: false,
-  };
-
-  it('should render the dropdown component correctly', () => {
-    const wrapper = shallow(<ProfileDropDown { ...props }/>);
+  const wrapper = shallow(<ProfileDropDown />);
+  it('should render the dropdown component correctly', () => {  
+    wrapper.setState({ dropDownHidden: false });
     expect(wrapper).toMatchSnapshot();
   });
+  
+  it('should close the dropdown modal onClick', () => {
+    wrapper.setState({ dropDownHidden: false });
+    wrapper.find('i').simulate('click');
+    expect(wrapper.find('div').length).toBe(0);
+  });
 
-  it('should not be rendered when the hidden props passed evaluates to false', () => {
-    props.hidden = true;
-    const wrapper = shallow(<ProfileDropDown { ...props } />);
-    expect(wrapper.contains(<div className="drop-down-item">Log Out</div>)).toBe(false);
+  it('should unmount the component when the log out button is clicked', () => {
+    wrapper.setState({ dropDownHidden: false });
+    const componentWillUnmount = jest.spyOn(wrapper.instance(), 'componentWillUnmount');
+    wrapper.find('div.dropdown-menu__item').simulate('click');
+    wrapper.unmount();
+    expect(componentWillUnmount).toHaveBeenCalled();
   });
 });
--- src/components/ProfileDropDown/index.tsx
+++ src/components/ProfileDropDown/index.tsx
@@ -7,20 +7,68 @@ import { Link } from 'react-router-dom';
 // styles
 import './ProfileDropDown.scss';
 
-// interfaces
-import { ProfileDropDownProps } from './interfaces';
-
 // helper functions
 import { authService } from 'utils/auth';
 
-const ProfileDropDown = (props: ProfileDropDownProps) => {
-  const paths = [
-    { label: 'Log Out', url: '/' },
-  ];
-  return (
-    !props.hidden &&
-    <div className="dropdown-menu">
-      {paths.map(({ url, label }, index) => (
+class ProfileDropDown extends React.Component<{}> {
+  private node?: HTMLElement;
+
+  state = {
+    dropDownHidden: true,
+  };
+
+  /**
+   * This is triggered when the component is un-mounted. It performs a clean up by terminating
+   * any calls to set-state via the toggleDropDownState method handler.
+   * 
+   * @returns {void}
+   */
+  componentWillUnmount() {
+    this.toggleDropDownState();
+  }
+
+  /**
+   * This handler changes the state of the dropDownHidden key
+   * in the state object. It also listens for click outside of the component
+   * and reacts depending on the state.
+   * 
+   * @returns {void}
+   */
+  toggleDropDownState = () => {
+    if (this.state.dropDownHidden) {
+      document.addEventListener('click', this.handleOutsideClick);
+    } else {
+      document.removeEventListener('click', this.handleOutsideClick);
+    }
+    this.setState({ dropDownHidden: !this.state.dropDownHidden });
+  }
+
+  /**
+   * This handler handles click outside of the ProfileDropDown component.
+   * 
+   * @returns {void}
+   */
+  handleOutsideClick = () => {
+    this.toggleDropDownState();
+  }
+
+  render() {
+    const { dropDownHidden } = this.state;
+    const paths = [
+      { label: 'Log Out', url: '/' },
+    ];
+
+    return (
+      <React.Fragment>
+        <i
+          ref={ node => this.node = node }
+          onClick={this.toggleDropDownState}
+          className="dropdown-icon">&#9662;
+        </i>
+        {!dropDownHidden && 
+          <div className="dropdown-menu">
+          {
+            paths.map(({ url, label }, index) => (
             <Link
               key={index}
               to={url}
@@ -31,9 +79,13 @@ const ProfileDropDown = (props: ProfileDropDownProps) => {
                 onClick={label === 'Log Out' && authService.logoutUser}
               >{label}</div>
             </Link>
-          ))}
-    </div>
-  );
-};
+          ))
+          }
+          </div>
+        }
+      </React.Fragment>
+    );
+  }
+}
 
 export default ProfileDropDown;
